6a54bd62ddd9800a0e898ff5799c479c3c989216
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2022-06-25 17:02:45 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+document.querySelector(".boton").addEventListener("click", myFunction);
+function myFunction() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let tipos = ["♦", "♥", "♠", "♣"];
+  let numelet = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let primero = Math.floor(Math.random() * tipos.length);
+  let segundo = Math.floor(Math.random() * numelet.length);
+
+  document.querySelector(".figura").innerHTML = tipos[primero];
+  document.querySelector(".figura2").innerHTML = tipos[primero];
+  document.querySelector(".numero").innerHTML = numelet[segundo];
+  document.querySelector(".numero2").innerHTML = numelet[segundo];
+  if (primero == 0 || primero == 1) {
+    document.querySelector(".figura").style.color = "red";
+    document.querySelector(".figura2").style.color = "red";
+  } else {
+    document.querySelector(".figura").style.color = "black";
+    document.querySelector(".figura2").style.color = "black";
+  }
+}
